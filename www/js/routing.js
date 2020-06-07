dc4d99651f9ba4ac5dc1e650915bce29bbedec0d
jackjack.japan1 <kr49kr21@gmail.com>
2020-06-07 12:12:11 +0900

    Edit ons-page.id, removing the part of *-group and use abbreviation.

--- www/js/routing.js
+++ www/js/routing.js
@@ -1,24 +1,27 @@
 'use strict'
 document.addEventListener('init', function (event) {
-  var page = event.target;
+  const page = event.target;
   if (page.id === 'entry') {
     page.querySelector('#js-move-home-btn').onclick = () => {
-      document.querySelector('#myNavigator').pushPage("../templates/home/home-recommended-group.html");
+      moveToHomeRcmddPage();
     };
-  } else if (page.id === 'home-recommended-group') {
+
+  } else if (page.id === 'home-rcmdd') {
     page.querySelector('#js-home-arr-g-btn').onclick = () => {
       moveToHomeArrPage();
     };
     page.querySelector('#js-home-now-g-btn').onclick = () => {
-      document.querySelector('#myNavigator').pushPage('../templates/home/home-now-group.html', { animation: 'fade' });
+      moveToHomeNowPage();
     };
-  } else if (page.id === 'home-arrival-group') {
+
+  } else if (page.id === 'home-arr') {
     page.querySelector('#js-home-rcmdd-g-btn').onclick = () => {
       moveToHomeRcmddPage();
     };
     page.querySelector('#js-home-now-g-btn').onclick = () => {
       moveToHomeNowPage();
     };
+
   } else if (page.id === 'home-now-group') {
     page.querySelector('#js-home-rcmdd-g-btn').onclick = () => {
       moveToHomeRcmddPage()
