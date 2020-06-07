36cd47e6f85ae8bdfb5b35fcf3cd8b12a470c390
jackjack.japan1 <kr49kr21@gmail.com>
2020-06-07 15:14:49 +0900

    Add push page by clicking header tabs and footer tabs.

--- www/js/routing.js
+++ www/js/routing.js
@@ -10,39 +10,62 @@ document.addEventListener('init', function (event) {
     page.querySelector('#js-move-home-btn').onclick = () => {
       moveToHomeRcmddPage();
     };
-  } else if (page.id === 'home-rcmdd') {
+  } else if (page.id === 'home-arr' || 'home-now' || 'home-rcmdd') {
+    // === home header tabs ===
     page.querySelector('#js-home-arr-btn').onclick = () => {
       moveToHomeArrPage();
     };
     page.querySelector('#js-home-now-btn').onclick = () => {
       moveToHomeNowPage();
     };
-  } else if (page.id === 'home-arr') {
     page.querySelector('#js-home-rcmdd-btn').onclick = () => {
       moveToHomeRcmddPage();
     };
-    page.querySelector('#js-home-now-btn').onclick = () => {
-      moveToHomeNowPage();
+    
+    // === home footer tabs ===
+    page.querySelector("#js-group-tab").onclick = () => {
+      moveToGroupPage();
     };
-  } else if (page.id === 'home-now-group') {
-    page.querySelector('#js-home-rcmdd-btn').onclick = () => {
-      moveToHomeRcmddPage()
+    page.querySelector("#js-home-tab").onclick = () => {
+      moveToHomeRcmddPage();
     };
-    page.querySelector('#js-home-arr-btn').onclick = () => {
-      moveToHomeArrPage();
+    page.querySelector("#js-match-making-tab").onclick = () => {
+      moveToMatchMakingPage();
+    };
+    page.querySelector("#js-schedule-tab").onclick = () => {
+      moveToSchedulePage();
+    };
+    page.querySelector("#js-topic-tab").onclick = () => {
+      moveToTopicPage();
     };
   }
 });
 
 // methods declaration
+const moveToGroupPage = (animation = 'fade') => {
+  myNavigtor.pushPage('/templates/group.html', { animation });
+};
+
 const moveToHomeArrPage = (animation = 'fade') => {
-  myNavigtor.pushPage('../templates/home/home-arrival-group.html', { animation });
+  myNavigtor.pushPage('/templates/home/home-arrival-group.html', { animation });
 };
 
 const moveToHomeNowPage = (animation = 'fade') => {
-  myNavigtor.pushPage('../templates/home/home-now-group.html', { animation });
+  myNavigtor.pushPage('/templates/home/home-now-group.html', { animation });
 };
 
 const moveToHomeRcmddPage = (animation = 'fade') => {
-  myNavigtor.pushPage('../templates/home/home-recommended-group.html', { animation });
+  myNavigtor.pushPage('/templates/home/home-recommended-group.html', { animation });
+};
+
+const moveToMatchMakingPage = (animation = 'fade') => {
+  myNavigtor.pushPage('/templates/match-making.html', { animation });
+};
+
+const moveToSchedulePage = (animation = 'fade') => {
+  myNavigtor.pushPage('/templates/schedule.html', { animation });
+};
+
+const moveToTopicPage = (animation = 'fade') => {
+  myNavigtor.pushPage('/templates/topic.html', { animation });
 };
