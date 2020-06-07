f1ff9f011e95f02a8c56187e4b1b8fecf829e649
jackjack.japan1 <kr49kr21@gmail.com>
2020-06-07 13:09:26 +0900

    Edit home-*.html to fix positon of headers, that are sticked the top of pages as changed.

--- www/js/routing.js
+++ www/js/routing.js
@@ -1,11 +1,15 @@
 'use strict'
+// vars declaration
+let myNavigtor;
+
+// events declaration
 document.addEventListener('init', function (event) {
   const page = event.target;
+  myNavigtor= document.querySelector('#myNavigator');
   if (page.id === 'entry') {
     page.querySelector('#js-move-home-btn').onclick = () => {
       moveToHomeRcmddPage();
     };
-
   } else if (page.id === 'home-rcmdd') {
     page.querySelector('#js-home-arr-btn').onclick = () => {
       moveToHomeArrPage();
@@ -13,7 +17,6 @@ document.addEventListener('init', function (event) {
     page.querySelector('#js-home-now-btn').onclick = () => {
       moveToHomeNowPage();
     };
-
   } else if (page.id === 'home-arr') {
     page.querySelector('#js-home-rcmdd-btn').onclick = () => {
       moveToHomeRcmddPage();
@@ -21,7 +24,6 @@ document.addEventListener('init', function (event) {
     page.querySelector('#js-home-now-btn').onclick = () => {
       moveToHomeNowPage();
     };
-
   } else if (page.id === 'home-now-group') {
     page.querySelector('#js-home-rcmdd-btn').onclick = () => {
       moveToHomeRcmddPage()
@@ -32,15 +34,15 @@ document.addEventListener('init', function (event) {
   }
 });
 
-const moveToHomeArrPage = () => {
-  document.querySelector('#myNavigator').pushPage('../templates/home/home-arrival-group.html', { animation: 'fade' });
+// methods declaration
+const moveToHomeArrPage = (animation = 'fade') => {
+  myNavigtor.pushPage('../templates/home/home-arrival-group.html', { animation });
 };
 
-const moveToHomeRcmddPage = () => {
-  document.querySelector('#myNavigator').pushPage('../templates/home/home-recommended-group.html', { animation: 'fade' });
+const moveToHomeNowPage = (animation = 'fade') => {
+  myNavigtor.pushPage('../templates/home/home-now-group.html', { animation });
 };
 
-const moveToHomeNowPage = () => {
-  document.querySelector('#myNavigator').pushPage('../templates/home/home-now-group.html', { animation: 'fade' });
+const moveToHomeRcmddPage = (animation = 'fade') => {
+  myNavigtor.pushPage('../templates/home/home-recommended-group.html', { animation });
 };
-
