2f821c423d96de1dfa61c119be059a81a4495060
jackjack.japan1 <kr49kr21@gmail.com>
2020-06-07 16:25:07 +0900

    Edit file path to read it on monaca debugger.

--- www/js/routing.js
+++ www/js/routing.js
@@ -52,29 +52,29 @@ document.addEventListener('init', function (event) {
 
 // methods declaration
 const moveToGroupPage = (animation = 'fade') => {
-  myNavigtor.pushPage('/templates/group.html', { animation });
+  myNavigtor.pushPage('templates/group.html', { animation });
 };
 
 const moveToHomeArrPage = (animation = 'fade') => {
-  myNavigtor.pushPage('/templates/home/home-arrival-group.html', { animation });
+  myNavigtor.pushPage('templates/home/home-arrival-group.html', { animation });
 };
 
 const moveToHomeNowPage = (animation = 'fade') => {
-  myNavigtor.pushPage('/templates/home/home-now-group.html', { animation });
+  myNavigtor.pushPage('templates/home/home-now-group.html', { animation });
 };
 
 const moveToHomeRcmddPage = (animation = 'fade') => {
-  myNavigtor.pushPage('/templates/home/home-recommended-group.html', { animation });
+  myNavigtor.pushPage('templates/home/home-recommended-group.html', { animation });
 };
 
 const moveToMatchMakingPage = (animation = 'fade') => {
-  myNavigtor.pushPage('/templates/match-making.html', { animation });
+  myNavigtor.pushPage('templates/match-making.html', { animation });
 };
 
 const moveToSchedulePage = (animation = 'fade') => {
-  myNavigtor.pushPage('/templates/schedule.html', { animation });
+  myNavigtor.pushPage('templates/schedule.html', { animation });
 };
 
 const moveToTopicPage = (animation = 'fade') => {
-  myNavigtor.pushPage('/templates/topic.html', { animation });
+  myNavigtor.pushPage('templates/topic.html', { animation });
 };
