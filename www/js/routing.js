b3998856a753d218bc843b121ade46c582734cdb
jackjack.japan1 <kr49kr21@gmail.com>
2020-06-07 11:32:01 +0900

    Add 3 types of home layouts.

--- www/js/routing.js
+++ www/js/routing.js
@@ -2,18 +2,42 @@
 document.addEventListener('init', function (event) {
   var page = event.target;
   if (page.id === 'entry') {
-    console.log('----')
-    console.log('hoge')
-    page.querySelector('#js-move-home-btn').onclick = function () {
-      document.querySelector('#myNavigator').pushPage('../templates/home.html');
+    page.querySelector('#js-move-home-btn').onclick = () => {
+      document.querySelector('#myNavigator').pushPage("../templates/home/home-recommended-group.html");
     };
-  } else if (page.id === 'home') {
-    page.querySelector('ons-toolbar .center').innerHTML = "HOME";
-    page.querySelector('#js-home-arr-g-btn').onclick = function () {
-      document.querySelector('#myNavigator').pushPage('../templates/home/home-arrival-group.html');
+  } else if (page.id === 'home-recommended-group') {
+    page.querySelector('#js-home-arr-g-btn').onclick = () => {
+      moveToHomeArrPage();
+    };
+    page.querySelector('#js-home-now-g-btn').onclick = () => {
+      document.querySelector('#myNavigator').pushPage('../templates/home/home-now-group.html', { animation: 'fade' });
     };
   } else if (page.id === 'home-arrival-group') {
-    page.querySelector('ons-toolbar .center').innerHTML = "HOME";
-    console.log('foooooo')
+    page.querySelector('#js-home-rcmdd-g-btn').onclick = () => {
+      moveToHomeRcmddPage();
+    };
+    page.querySelector('#js-home-now-g-btn').onclick = () => {
+      moveToHomeNowPage();
+    };
+  } else if (page.id === 'home-now-group') {
+    page.querySelector('#js-home-rcmdd-g-btn').onclick = () => {
+      moveToHomeRcmddPage()
+    };
+    page.querySelector('#js-home-arr-g-btn').onclick = () => {
+      moveToHomeArrPage();
+    };
   }
 });
+
+const moveToHomeArrPage = () => {
+  document.querySelector('#myNavigator').pushPage('../templates/home/home-arrival-group.html', { animation: 'fade' });
+};
+
+const moveToHomeRcmddPage = () => {
+  document.querySelector('#myNavigator').pushPage('../templates/home/home-recommended-group.html', { animation: 'fade' });
+};
+
+const moveToHomeNowPage = () => {
+  document.querySelector('#myNavigator').pushPage('../templates/home/home-now-group.html', { animation: 'fade' });
+};
+
