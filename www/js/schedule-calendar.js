670db622f6f123ba94d7f15a25c14039e2c46b3e
jackjack.japan1 <kr49kr21@gmail.com>
2020-07-18 16:53:18 +0900

    Debug schedule-calendar.html that had load bugs, could not show calendar more than once. It still has problem that if you move to this page oon the second time or more, month shift is skipped then next month or the previous month. When implementing the prototype, we should change source code of monthly.js file that douloaded from github.

--- www/js/schedule-calendar.js
+++ www/js/schedule-calendar.js
@@ -1,9 +1,19 @@
 // 飲み会スケジュール画面: カレンダー
 document.addEventListener('init', function (event) {
   const page = event.target;
-  // === sample data set ===
   if (page.id != "schedule-calendar") {
     return
   }
-  $('#mycalendar').monthly();
+  $('#myCalendar').monthly();
+});
+
+document.addEventListener('hide', function (event) {
+  const page = event.target;
+  if (page.id !== "schedule-calendar") {
+    return
+  }
+  const myCalendar = document.getElementById("myCalendar");
+  if (myCalendar !== null) {
+    document.getElementById("myCalendar").remove(); // [NOTE]: 2回目のinit時に前のデータが残って表示できないバグがあるので、画面消滅時に要素を明示的にリムーブ
+  }
 });
